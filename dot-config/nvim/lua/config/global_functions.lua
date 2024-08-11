2ee9aba52a9fa00117c197bcc90e00835da24e7d
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-08-11 15:29:36 +0300

    Updated the LazyVim config

--- dot-config/nvim/lua/config/global_functions.lua
+++
@@ -1,8 +0,0 @@
-function RUN_COMMAND_IN_NEW_BUFFER()
-	local command = vim.fn.input("Command: ")
-	vim.cmd("new")
-	vim.bo.buftype = "nofile"
-	vim.bo.bufhidden = "hide"
-	vim.bo.swapfile = false
-	vim.cmd("read !" .. command)
-end
--- dot-config/nvim/lua/config/keymaps.lua
+++ dot-config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- vim.api.nvim_set_keymap("n", "<space>", ":colorscheme habamax", { noremap = true, silent = true })
-
-require("config.global_functions")
-
-vim.api.nvim_set_keymap("n", "<leader>C", ":lua RUN_COMMAND_IN_NEW_BUFFER()<CR>", { noremap = true, silent = false })
