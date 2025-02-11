f731db59757914bc44c0ad0bdbb89a84acdd51c7
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2025-02-11 18:32:35 +0200

    Changed `config` to `opts`

--- dot-config/nvim/lua/plugins/neo-tree.lua
+++ dot-config/nvim/lua/plugins/neo-tree.lua
@@ -2,12 +2,10 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    config = function()
-      require("neo-tree").setup({
-        window = {
-          position = "right",
-        },
-      })
-    end,
+    opts = {
+      window = {
+        position = "right",
+      },
+    },
   },
 }
