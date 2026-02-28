6b9a14dea893cf73a31d50ac3e72f876b8fedd47
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2026-02-28 21:40:57 +0200

    Update godot config

--- dot-config/nvim/lua/plugins/godot.lua
+++ dot-config/nvim/lua/plugins/godot.lua
@@ -1,7 +1,53 @@
 -- Credits:
 -- - https://mb-izzo.github.io/nvim-godot-solution/
 -- - https://simondalvai.org/blog/godot-neovim/
+-- - https://www.youtube.com/watch?v=cLWgjienc_s
+--
+-- NOTE:
+-- To use `nvim` as an external editor in Godot, supply the following arguments:
+-- - `--server ./godothost --remote-send "<C-\><C-N>:n {file}<CR>{line}G{col}|"`
 
 return {
-  require("lspconfig").gdscript.setup({}),
+  {
+    "habamax/vim-godot",
+    event = "BufReadPre *.gd",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.gdscript.setup({
+        capabilities = vim.lsp.protocol.make_client_capabilities(),
+      })
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        modules = {},
+        sync_install = false,
+        ignore_install = {},
+        ensure_installed = {
+          "gdscript",
+          "godot_resource",
+          "gdshader",
+          "bash",
+          "c",
+          "html",
+          "lua",
+          "markdown",
+          "vim",
+          "vimdoc",
+        },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }
