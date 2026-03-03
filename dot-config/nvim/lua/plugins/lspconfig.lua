1bb08bb56f1699bf1ef2ba406ce4d105658794c4
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2026-03-03 23:16:16 +0200

    Simplified lspconfig

--- dot-config/nvim/lua/plugins/lspconfig.lua
+++ dot-config/nvim/lua/plugins/lspconfig.lua
@@ -2,39 +2,6 @@ return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    local lspconfig = require("lspconfig")
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-    -- Python
-    lspconfig.pyright.setup({
-      capabilities = capabilities,
-      settings = {
-        python = {
-          analysis = {
-            typeCheckingMode = "basic",
-          },
-        },
-      },
-    })
-
-    -- Lua
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
-      },
-    })
-
-    -- Rust
-    lspconfig.rust_analyzer.setup({
-      capabilities = capabilities,
-    })
-
-    -- Global keymaps
     vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true })
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
     vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true })
