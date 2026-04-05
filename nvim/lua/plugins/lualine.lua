39d5787c1f1e57743a5134e308d62913f4dbf3cf
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2026-04-05 22:32:34 +0300

    Add Macro recording in Lualine
    I have no clue why this isn't the default, whatever.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -16,6 +16,29 @@ return {
     return {
       options = {
         theme = lualine_theme,
+        component_separators = "",
+        section_separators = "",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          "filename",
+          {
+            function()
+              local reg = vim.fn.reg_recording()
+              if reg == "" then
+                return ""
+              end
+              return "recording @" .. reg
+            end,
+            color = { fg = colors.hl_col, gui = "bold" },
+            padding = { left = 1 },
+          },
+        },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
     }
   end,
